f4005ad26b594466a4dcc155e505afe2894ed3d7
GuillaumeUVHC <croy.guillaume@outlook.fr>
2019-04-09 10:00:21 +0200

    correct layout width

---
+++ app/src/main/java/com/example/devmobilecroymorin/extendedFormView/ExtSwitch.kt
@@ -0,0 +1,6 @@
+package com.example.devmobilecroymorin.extendedFormView
+
+import android.content.Context
+import android.widget.Switch
+
+class ExtSwitch(val entry : String, context: Context?) : Switch(context)
